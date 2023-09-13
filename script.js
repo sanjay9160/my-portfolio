9e84c8adef50ec256d9b1bf432353a9a8c424db7
sanjay0619 <sanjayst0619@gmail.com>
2023-09-13 17:30:28 +0530

    updated-10

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-alert("Welcome to my Portfolio");
-
 function onClickMenu() {
     document.getElementById("menu").classList.toggle("icon");
     document.getElementById("sidebar").classList.toggle("change");
 };
+
+AOS.init({
+    delay: 400,
+    duration: 1000,
+    offset: 300,
+});
